6b44597178651dcbb4ce4c21dd2008dadf9f2d94
Konifar <yahpeycoy0403@gmail.com>
2014-02-15 21:11:19 +0900

    created rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JinjiChoseiSan::Application.config.secret_key_base = '8634b267da6e1cfedc998c7c6a2a45fe33223c91486a473e32e813e2de558e1dccb1ac3d0e0df436310d442aac21a2243948906bd64d043be7165f918402fcb4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JinjiChoseiSan::Application.config.session_store :cookie_store, key: '_jinji_chosei_san_session'
